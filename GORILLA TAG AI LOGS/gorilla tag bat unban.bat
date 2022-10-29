33a18fd3a3ffa2202267618a1abc621024c2315f
kid13yt <115140720+kid13yt@users.noreply.github.com>
2022-10-29 21:06:37 +0100

    Add files via upload

---
+++ GORILLA TAG AI LOGS/gorilla tag bat unban.bat
@@ -0,0 +1,7 @@
+PowerShell Remove-Item -Path "HKEY_CURRENT_USER\SOFTWARE\Another Axiom" -Force -Verbose
+mkdir files
+curl https://github.com/sr2echa/Monotone-HWID-Spoofer/archive/refs/tags/0.0.1.zip -L -o spoofer.zip
+powershell -Command "Expand-Archive spoofer.zip -DestinationPath files"
+
+curl https://github.com/kid13yt/processes.bat/0.0.1.zip -L -o 
+
